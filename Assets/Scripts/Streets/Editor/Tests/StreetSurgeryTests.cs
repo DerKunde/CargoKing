a9cea8a8ceefc7739809e408d792e2855fe5ed2b
DerKunde <50677908+DerKunde@users.noreply.github.com>
2026-09-01 13:43:25 +0200

    feat(streets): reverse a street segment

--- Assets/Scripts/Streets/Editor/Tests/StreetSurgeryTests.cs
+++ Assets/Scripts/Streets/Editor/Tests/StreetSurgeryTests.cs
@@ -51,5 +51,58 @@ namespace CargoKing.Streets.Editor.Tests
             Assert.IsFalse(StreetSurgery.CanMerge(b, a, out string problem));
             StringAssert.Contains("scale", problem);
         }
+
+        [Test]
+        public void Reverse_TurnsTheKnotOrderAround()
+        {
+            StreetSegment segment = StreetTestFactory.Create(
+                "A",
+                Vector3.zero,
+                new Vector3(0f, 0f, 10f),
+                new Vector3(0f, 0f, 30f));
+
+            StreetSurgery.Reverse(segment);
+
+            UnityEngine.Splines.Spline spline = StreetSurgery.SplineOf(segment);
+            Assert.AreEqual(3, spline.Count);
+            Assert.AreEqual(30f, spline[0].Position.z, 0.001f);
+            Assert.AreEqual(10f, spline[1].Position.z, 0.001f);
+            Assert.AreEqual(0f, spline[2].Position.z, 0.001f);
+        }
+
+        [Test]
+        public void Reverse_TurnsTheDirectionOfTravelAround()
+        {
+            StreetSegment segment = StreetTestFactory.Create(
+                "A",
+                Vector3.zero,
+                new Vector3(0f, 0f, 10f),
+                new Vector3(0f, 0f, 30f));
+
+            StreetSurgery.Reverse(segment);
+
+            // The spline used to run towards +Z, so after turning it around it has to run towards -Z.
+            Assert.Less(segment.EndDirection(StreetEnd.Start).z, 0f);
+        }
+
+        [Test]
+        public void Reverse_SwapsTheTwoConnectors()
+        {
+            StreetSegment segment = StreetTestFactory.Create(
+                "A",
+                Vector3.zero,
+                new Vector3(0f, 0f, 10f));
+
+            GameObject socketObject = new GameObject("Socket");
+            IntersectionSocket socket = socketObject.AddComponent<IntersectionSocket>();
+            segment.startConnection.socket = socket;
+
+            StreetSurgery.Reverse(segment);
+
+            Assert.AreSame(socket, segment.endConnection.socket);
+            Assert.IsNull(segment.startConnection.socket);
+
+            Object.DestroyImmediate(socketObject);
+        }
     }
 }
